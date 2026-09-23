355aad3529768277d6df9bb53355e14151c729cc
anisaasaluka <anisaasaluka@gmail.com>
2026-09-23 22:21:08 +0700

    Add files via upload

---
+++ pertemuan6/latihan2_P6.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan 2 Array</title>
+</head>
+
+<body>
+
+<?php
+
+$x = array("one", "two", "three");
+
+foreach ($x as $value)
+{
+    echo $value . "<br />";
+}
+
+?>
+
+</body>
+</html>
